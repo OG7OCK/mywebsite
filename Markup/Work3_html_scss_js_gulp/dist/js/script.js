b23cecfdf80a6817d05ae8ec8b32cbc59c70664b
Oleksandr <og7ock@gmail.com>
2021-08-25 15:43:44 +0300

    2

--- Markup/Work3_html_scss_js_gulp/dist/js/script.js
+++ Markup/Work3_html_scss_js_gulp/dist/js/script.js
@@ -60,28 +60,28 @@ if (iconBurger2){
 
 function Tabs() {
     var bindAll = function() {
-      var menuElements = document.querySelectorAll('[data-tab]');
-      for(var i = 0; i < menuElements.length ; i++) {
+    var menuElements = document.querySelectorAll('[data-tab]');
+    for(var i = 0; i < menuElements.length ; i++) {
         menuElements[i].addEventListener('click', change, false);
-      }
     }
-  
+    }
+
     var clear = function() {
-      var menuElements = document.querySelectorAll('[data-tab]');
-      for(var i = 0; i < menuElements.length ; i++) {
+    var menuElements = document.querySelectorAll('[data-tab]');
+    for(var i = 0; i < menuElements.length ; i++) {
         menuElements[i].classList.remove('active');
         var id = menuElements[i].getAttribute('data-tab');
         document.getElementById(id).classList.remove('active');
-      }
     }
-  
+    }
+
     var change = function(e) {
-      clear();
-      e.target.classList.add('active');
-      var id = e.currentTarget.getAttribute('data-tab');
-      document.getElementById(id).classList.add('active');
+    clear();
+    e.target.classList.add('active');
+    var id = e.currentTarget.getAttribute('data-tab');
+    document.getElementById(id).classList.add('active');
     }
-  
+
     bindAll();
 }
 var connectTabs = new Tabs();
@@ -129,11 +129,11 @@ var swiper2 = new Swiper('.swiper-container.two',{
     centeredSlides:true,
     breakpoints: {
         320: {
-          slidesPerView: 1,
+        slidesPerView: 1,
         },
         1195: {
             slidesPerView: 3,
-          },
+        },
     }
 });
 
@@ -152,11 +152,11 @@ var swiper3 = new Swiper('.swiper-container',{
     centeredSlides:true,
     breakpoints: {
         320: {
-          slidesPerView: 1,
+        slidesPerView: 1,
         },
         458: {
             slidesPerView: 1.5,
-          },
+        },
     }
 });
 
